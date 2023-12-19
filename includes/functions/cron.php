547a68f02282a3cf46c252a7bbacd400675eca54
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-12-19 12:17:09 +0200

    Update cron.php

--- includes/functions/cron.php
+++ includes/functions/cron.php
@@ -6,22 +6,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
-// Schedule hourly event to update stores list.
-if (!wp_next_scheduled('linkedevents_cron_hook')) {
-    wp_schedule_event(time(), 'hourly', 'linkedevents_cron_hook');
+add_filter( 'linked_events_cron_hook_name', __NAMESPACE__ . '\\cron_hook_name' );
+function cron_hook_name(): string {
+	return 'linkedevents_cron_hook';
 }
-add_action('linkedevents_cron_hook', '\Evermade\LinkedEvents\updateStores');
 
-
-
-/**
- * Update stores.
- *
- * @return void
- */
-function updateStores() {
-
-    $hyperIn = new \Evermade\LinkedEvents\LinkedEvents();
-    $stores = $hyperIn->updateStores();
-
+// Schedule hourly event to update stores list.
+if ( ! wp_next_scheduled( cron_hook_name() ) ) {
+    wp_schedule_event( time(), 'hourly', cron_hook_name() );
 }
