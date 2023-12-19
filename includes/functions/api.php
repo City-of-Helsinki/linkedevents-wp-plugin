bfeb86a96a04201ee97ae288662ffd93f3dba288
Antti Koskinen <koskinenaa@users.noreply.github.com>
2023-12-19 12:17:13 +0200

    Update api.php

--- includes/functions/api.php
+++ includes/functions/api.php
@@ -2,24 +2,75 @@
 
 namespace Evermade\LinkedEvents\Api;
 
+use Evermade\LinkedEvents\LinkedEvents;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	die();
 }
 
+add_action( 'init', __NAMESPACE__ . '\\handle_events_and_actions' );
+function handle_events_and_actions(): void {
+	$cron = apply_filters( 'linked_events_cron_hook_name', '' );
+	if ( $cron ) {
+		add_action( $cron, __NAMESPACE__ . '\\update_stores' );
+	}
+
+	add_action( 'admin_post_' . admin_sync_action(), __NAMESPACE__ . '\\sync_events' );
+}
+
+add_filter( 'linked_events_api', __NAMESPACE__ . '\\provide_linked_events_api' );
+function provide_linked_events_api(): LinkedEvents
+{
+	return new LinkedEvents( array(
+		'tprek_id' => apply_filters( 'linked_events_tprek_id', '' ),
+		'transient_name' => 'linkedevents-events',
+		'api_url' => 'https://api.hel.fi/linkedevents/v1',
+	) );
+}
+
+/**
+  * Admin action
+  */
+function admin_sync_action(): string {
+	return 'sync_linked_events';
+}
+
+function sync_events(): void {
+	update_stores();
+
+	wp_redirect( add_query_arg(
+        array( 'page' => apply_filters( 'linked_events_settings_page_slug', '' ) ),
+        admin_url( 'admin.php' )
+    ) );
+	die;
+}
+
+add_action( 'linked_events_settings_page', __NAMESPACE__ . '\\sync_events_button', 100 );
+function sync_events_button(): void {
+	printf(
+		'<a class="button" href="%s">%s</a>',
+		esc_url( add_query_arg(
+	        array( 'action' => admin_sync_action() ),
+	        admin_url( 'admin-post.php' )
+	    ) ),
+		__( 'Sync events', 'linkedevents' )
+	);
+}
 
 /**
  * Get all stores. Not pagination or any other fancy stuff since there are not that many items.
  *
  * @return void
  */
-function updateStores() {
+function update_stores() {
+    $hyperIn = apply_filters( 'linked_events_api', null );
+	if ( $hyperIn ) {
+		$hyperIn->updateStores();
 
-    // Get stores.
-    $hyperIn = new \Evermade\LinkedEvents\LinkedEvents();
-    $stores = $hyperIn->updateStores();
-
-    return ['success' => true];
-
+		return rest_ensure_response( array( 'success' => true ) );
+	} else {
+		return rest_ensure_response( array( 'success' => false ) );
+	}
 }
 
 
@@ -28,16 +79,16 @@ function updateStores() {
  *
  * @return void
  */
-function getStores() {
+function get_stores() {
+	$hyperIn = apply_filters( 'linked_events_api', null );
+	if ( ! $hyperIn ) {
+		return rest_ensure_response( array() );
+	}
 
     // Get stores.
-    $hyperIn = new \Evermade\LinkedEvents\LinkedEvents();
-    $stores = $hyperIn->getStores(true);
-
-    // print_r($stores);exit;
-
-    if (!$stores) {
-        return [];
+    $stores = $hyperIn->getStores();
+    if ( ! $stores ) {
+        return rest_ensure_response( array() );
     }
 
     // Do some magic to transform API respose to be like WordPress post object.
@@ -70,8 +121,8 @@ function getStores() {
             )
         ]);
     });
-    return $storesWp;
 
+	return rest_ensure_response( $storesWp );
 }
 
 
@@ -80,12 +131,12 @@ add_action('rest_api_init', function () {
 
     register_rest_route( 'linkedevents/v1', '/updateevents', array(
         'methods' => 'GET',
-        'callback' => '\Evermade\LinkedEvents\Api\updateStores',
+        'callback' => __NAMESPACE__ . '\\update_stores',
     ));
 
     register_rest_route( 'linkedevents/v1', '/events', array(
         'methods' => 'GET',
-        'callback' => '\Evermade\LinkedEvents\Api\getStores',
+        'callback' => __NAMESPACE__ . '\\get_stores',
     ));
 
 });
